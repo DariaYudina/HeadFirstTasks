9e4ff2945dca6ddc056765120737119806fddaa1
dariayudina <dashaudina06@gmail.com>
2023-12-02 18:51:59 +0400

    add game

---
+++ AnimalsGame/AnimalsGame/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimalsGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalsEmoji = new List<string>()
+            {
+                "👻","👻",
+                "🦄","🦄",
+                "🦁","🦁",
+                "🐺","🐺",
+                "🐳","🐳",
+                "🐶","🐶",
+                "🦝","🦝",
+                "🐼","🐼"
+            };
+
+            Random rnd = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = rnd.Next(animalsEmoji.Count);
+                string nextEmoji = animalsEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalsEmoji.RemoveAt(index);
+            }
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+    }
+}
